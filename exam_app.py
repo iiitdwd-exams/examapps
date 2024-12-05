82a7b4f08f3ce8b97ee2559c9d78dbe576f0df53
Satish Annigeri <satish.annigeri@gmail.com>
2024-12-06 00:54:55 +0530

    Added pdfcrack to main app

--- exam_app.py
+++ exam_app.py
@@ -9,11 +9,17 @@ home_page = st.Page(
 
 suppl_exam_page = st.Page(
     "suppl_exam.py",
-    title="Supplementary Exam Statistics",
+    title="Supplementary Exam Stats",
     icon=":material/book:",
 )
 
-pages = [home_page, suppl_exam_page]
+pdfcrack_page = st.Page(
+    "pdfcrack.py",
+    title="Remove PDF password",
+    icon=":material/no_encryption:",
+)
+
+pages = [home_page, suppl_exam_page, pdfcrack_page]
 page_dict = {"IIIT Dharwad": pages}
 pg = st.navigation(page_dict)
 pg.run()
