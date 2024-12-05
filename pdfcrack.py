bb3ae22ee53549ae99d00924e16c74f73098ea80
Satish Annigeri <satish.annigeri@gmail.com>
2024-12-06 00:49:00 +0530

    Added pdfcrack.py to remove password from PDF files

--- pdfcrack.py
+++ pdfcrack.py
@@ -1,41 +1,32 @@
-# from pathlib import Path
+from pathlib import Path
+import uuid
 import streamlit as st
 import pikepdf
 
 
-def remove_password_from_pdf(fname: str, password: str, output_fname: str = ""):
-    if not output_fname:
-        output_fname = fname
-        allow = True
-    else:
-        allow = False
-    pdf = pikepdf.open(fname, password=password, allow_overwriting_input=allow)
-    pdf.save(output_fname)
+uploaded_pdf = st.file_uploader("Password protected PDF file", type="pdf")
+password = st.text_input("Password to open PDF file", value="ACWPA7045H")
+tmp_pdf_name = f"{str(uuid.uuid4())}.pdf"
+tmp_pdf_path = f"/home/satish/{tmp_pdf_name}"
+if st.button("Remove password"):
+    if uploaded_pdf is not None and password:
+        try:
+            # Open the uploaded file object in pikepdf
+            decrypted_pdf = pikepdf.open(uploaded_pdf, password=password)
+            # Save the decrypted PDF to a temporary file
+            decrypted_pdf.save(tmp_pdf_path)
+            st.success("Password removed successfully")
+            # Download the saved decrypted file to local filesystem
+            with open(tmp_pdf_path, "rb") as data:
+                btn = st.download_button(
+                    label="Download decrypted PDF file",
+                    data=data,
+                    file_name=uploaded_pdf.name,
+                    mime="application/pdf",
+                )
+            fpath = Path(tmp_pdf_path)
+            if fpath.exists():
+                fpath.unlink()
 
-
-def overwrite_onchange():
-    if overwrite:
-        output_file = st.text_input("Name of cracked PDF file", fname, disabled=True)
-    else:
-        output_file = st.text_input("Name of cracked PDF file", fname, disabled=False)
-    return output_file
-
-
-if "pdf_overwrite" not in st.session_state:
-    st.session_state.pdf_overwrite = True
-if "pdf_input" not in st.session_state:
-    pdf_input = ""
-
-
-uploaded_file = st.file_uploader("Password protected PDF file", "pdf")
-if uploaded_file is not None:
-    fname = st.session_state.pdf_input = uploaded_file.name
-    password = st.text_input("Password")
-    overwrite = st.checkbox("Overwrite file", value=st.session_state.pdf_overwrite)
-    pdf_output = st.text_input(
-        "Name of cracked PDF file",
-        fname,
-        disabled=st.session_state.pdf_overwrite,
-    )
-else:
-    st.stop()
+        except Exception as e:
+            st.error(f"Failed to remove password: {e}")
